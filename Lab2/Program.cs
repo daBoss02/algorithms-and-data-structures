a82c0272a4737aa0e9d7e5b5d628874c22125821
Seth Vandenbos <seth.vdbos@gmail.com>
2023-01-13 17:36:15 -0600

    Questions 4 and 5 done

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+    
+        For example, the string “Programmatic Python” would result in the array ['p','r','o','a','m','t'].
+        For example, the string “To be or not to be” returns ["to","be","or","not"].
+        For example, the string "Tiptoe through the tulips" would print "through"
+        If there are multiple words tied for greatest length, print the last one
+*/
+
+string testSentence = "Tiptoe through the tulips";
+
+// A program that produces an array of all of the characters that appear more than once in a string.
+
+Console.WriteLine("\nProducing an array of repeated characters\n");
+
+// A program returns an array of strings that are unique words found in the argument.
+
+Console.WriteLine("\nProducing an array of unique words\n");
+
+// A program that reverses a provided string
+
+Console.WriteLine("\nReversing a string\n");
+
+Console.WriteLine("Please input a phrase");
+string nonReversed = Console.ReadLine();
+char[] reversed = new char[nonReversed.Length];
+for (int i = reversed.Length - 1;i >= 0; i--)
+{
+    int newIndex = reversed.Length - 1 - i;
+    reversed[newIndex] = nonReversed[i];
+
+}
+
+Console.WriteLine(string.Join("", reversed));
+
+// A program that finds the longest unbroken word in a string and prints it
+
+Console.WriteLine("\nFinding the longest unbroken word in a sentence\n");
+
+Console.WriteLine("Please input a phrase");
+string[] wordArray = Console.ReadLine().Split(' ');
+
+string longestWord = "";
+
+foreach (string word in wordArray)
+{
+    if (word.Length >= longestWord.Length)
+    {
+        longestWord = word;
+    }
+}
+
+Console.WriteLine($"{longestWord} is the longest word in this text");
