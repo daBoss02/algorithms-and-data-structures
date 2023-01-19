408654f07f2f01a76a8ccfb9f9014fd91e33c810
Seth Vandenbos <seth.vdbos@gmail.com>
2023-01-18 22:44:01 -0600

    Questions 2 and 3 completed

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -6,6 +6,8 @@
         Output the results with a message like: “List 1 has a maximum of 5. List 2 has a maximum of 9. List 3 has a maximum of 2.”
 */
 
+Console.WriteLine("Getting the maximums");
+
 List<List<int>> numsList = new List<List<int>>();
 
 numsList.Add(new List<int>() { 1, 5, 3 });
@@ -14,6 +16,7 @@ numsList.Add(new List<int>() { 2, 1, 2 });
 
 List<int> MaxNumbersInLists(List<List<int>> numLists)
 {
+    // O(n)^2
     List<int> maxNums = new List<int>();
     for (int i = 0; i < numLists.Count; i++)
     {
@@ -30,7 +33,12 @@ List<int> MaxNumbersInLists(List<List<int>> numLists)
     return maxNums;
 }
 
-Console.WriteLine(MaxNumbersInLists(numsList));
+List<int> maxes = MaxNumbersInLists(numsList);
+
+for (int i = 0; i < maxes.Count; i++)
+{
+    Console.WriteLine($"List {i + 1} has a maximum of {maxes[i]}.");
+}
 
 /*
     String HighestGrade(List<List<int>>) accepts a list of number grades among students in different courses (where each list represents a single course), between 0 and 100. It returns the highest grade among all students in all courses.
@@ -38,8 +46,68 @@ Console.WriteLine(MaxNumbersInLists(numsList));
         For example: { {85,92, 67, 94, 94}, {50, 60, 57, 95}, {95} } returns "The highest grade is 95. This grade was found in class(es) {index}".
 */
 
+Console.WriteLine("Finding the Highest Grade");
+List<List<int>> grades = new List<List<int>>() ;
+grades.Add(new List<int> { 85, 92, 67, 94, 94 });
+grades.Add(new List<int> { 50, 60, 57, 95 });
+grades.Add(new List<int> { 95 });
+String HighestGrade(List<List<int>> gradesList)
+{
+    List<int> courses = new List<int>();
+    int highestGrade = 0;
+    for (int i = 0; i < gradesList.Count; i++)
+    {
+        foreach (int num in gradesList[i])
+        {
+            if (num > highestGrade)
+            {
+                highestGrade = num;
+                courses.Clear();
+                courses.Add(i);
+            } else if (num == highestGrade)
+            {
+                courses.Add(i);
+            }
+        }
+    }
+    return $"The highest grade is {highestGrade}. This grade was found in class(es) {string.Join(", ", courses)}";
+}
+
+
+Console.WriteLine(HighestGrade(grades));
 /*
     List<int> OrderByLooping (List<int>) orders a list of integers, from least to greatest, using only basic control statements (ie. if/else, for/while).
 
         For example, argument {6, -2, 5, 3} returns {-2, 3, 5, 6}.
 */
+
+List<int> randomNums = new List<int>() { 47, -98, -12, 13, 2, 10 };
+
+List<int> OrderByLooping(List<int> intList)
+{
+    for (int i = 1; i < intList.Count; i++)
+    {
+        int previous = intList[i - 1];
+        int current = intList[i];
+        int count = 1;
+
+            Console.WriteLine(current < previous);
+        while (current < previous)
+        {
+            Console.WriteLine(current);
+            Console.WriteLine(previous);
+            intList[i - count] = current;
+            intList[i - count + 1] = previous;
+            count++;
+            if (i - count < 0)
+            {
+                break;
+            }
+                previous = intList[i - count];
+        }
+
+    }
+    return intList;
+}
+
+Console.WriteLine(string.Join(", ", OrderByLooping(randomNums)));
